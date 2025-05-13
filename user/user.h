3563458394186aacb79d63cdc7c6745cddf9d0d3
rhit-hayesof <hayesof@rose-hulman.edu>
2025-05-12 22:40:32 -0400

    user.h fix

--- user/user.h
+++ user/user.h
@@ -1,3 +1,8 @@
+#ifndef USER_H
+#define USER_H
+
+typedef unsigned int uint;
+
 struct stat;
 
 // system calls
@@ -41,3 +46,5 @@ void free(void*);
 int atoi(const char*);
 int memcmp(const void *, const void *, uint);
 void *memcpy(void *, const void *, uint);
+
+#endif // USER_H
